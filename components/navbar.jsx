f8fdb27bdc5e52d7e85ad9155b46993751fcead1
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-11-21 09:28:54 +0700

    Navbar responsif

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,6 +3,8 @@ import Logo from "../public/logo.png";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 import { Fragment } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 export default function navbar() {
   const { data: session, status } = useSession();
@@ -11,17 +13,22 @@ export default function navbar() {
     signOut();
   };
 
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <Fragment>
       <div className="flex justify-around items-center py-4 bg-[#4B4B4B] bg-opacity-80 m-8">
         {/* <a href="/">
         <Image src={Logo} alt="logo" width={250} height={70} />
-      </a> */}
+        </a> */}
         <Link href="/">
-          <Image src={Logo} alt="logo" width={250} height={70} />
+          <Image src={Logo} alt="logo" className=" w-[250px] h-[70px]" />
         </Link>
         <div>
-          <ul className="flex justify-evenly gap-12">
+          <ul className="hidden md:flex justify-evenly gap-12">
             {session && (
               <li>
                 <Link
@@ -48,7 +55,7 @@ export default function navbar() {
           {!session && status !== "loading" && (
             <Link
               href="/authentication/login"
-              className="text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-8 py-2 hover:bg-[#FFD700] hover:text-black"
+              className="hidden md:block text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-8 py-2 hover:bg-[#FFD700] hover:text-black"
             >
               Login
             </Link>
@@ -56,19 +63,100 @@ export default function navbar() {
           {!session && status !== "loading" && (
             <Link
               href="/authentication/register"
-              className="text-white font-sans border border-solid border-white rounded-lg px-6 py-2 hover:bg-white hover:text-black"
+              className="hidden md:block text-white font-sans border border-solid border-white rounded-lg px-6 py-2 hover:bg-white hover:text-black"
             >
               Register
             </Link>
           )}
           {session && (
             <button
-              className="text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-8 py-2 hover:bg-[#FFD700] hover:text-black"
+              className="hidden md:block text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-8 py-2 hover:bg-[#FFD700] hover:text-black"
               onClick={logoutHandler}
             >
               Logout
             </button>
           )}
+
+          <div onClick={handleNav} className="block lg:hidden z-[110]">
+            {nav ? (
+              <AiOutlineClose
+                className="z-[110]"
+                size={30}
+                style={{ color: `white` }}
+              />
+            ) : (
+              <AiOutlineMenu style={{ color: "white" }} size={30} />
+            )}
+          </div>
+          <div
+            className={`lg:hidden fixed z-[100] top-0 ${
+              nav ? "translate-x-0" : "translate-x-full"
+            } transition duration-300 right-0 bottom-0 flex justify-center items-center w-[80vw] h-screen bg-[#4B4B4B] text-center text-white`}
+          >
+            <ul>
+              {session && (
+                <li onClick={handleNav} className="p-4">
+                  <Link
+                    href="/user/member/userid"
+                    className="hover:opacity-70 hover:text-[#FFD700]"
+                  >
+                    My Profile
+                  </Link>
+                </li>
+              )}
+              <li onClick={handleNav} className="p-4">
+                <Link
+                  href="/classes"
+                  className="hover:opacity-70 hover:text-[#FFD700]"
+                >
+                  Classes
+                </Link>
+              </li>
+              <li onClick={handleNav} className="p-4">
+                <Link
+                  href="/admin"
+                  className="hover:opacity-70 hover:text-[#FFD700]"
+                >
+                  Admin
+                </Link>
+              </li>
+
+              {!session && (
+                <ul>
+                  <li onClick={handleNav} className="p-4">
+                    <Link
+                      href="/authentication/login"
+                      className="hover:opacity-70 hover:text-[#FFD700]"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li onClick={handleNav} className="p-4">
+                    <Link
+                      href="/authentication/register"
+                      className="hover:opacity-70 hover:text-[#FFD700]"
+                    >
+                      Register
+                    </Link>
+                  </li>{" "}
+                </ul>
+              )}
+
+              {session && (
+                <ul>
+                  <li onClick={handleNav} className="p-4">
+                    <Link
+                      href="/"
+                      OnClick={logoutHandler}
+                      className="hover:opacity-70 hover:text-[#FFD700]"
+                    >
+                      Logout
+                    </Link>
+                  </li>{" "}
+                </ul>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </Fragment>
