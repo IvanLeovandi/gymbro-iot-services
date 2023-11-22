ba6cbabc5e821ebe314e8c829e9b726e9f3b27bf
Wentonn <nicolas.owen@gmail.com>
2023-11-22 11:17:20 +0700

    userpic biocard

--- components/userpic.jsx
+++ components/userpic.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import Card from "../public/card.png";
+import Image from "next/image";
 
 export default function Userpic(props) {
   // const [role, setRole] = useState("");
@@ -34,16 +36,19 @@ export default function Userpic(props) {
 
   return (
     <Fragment>
-      <div className="bg-gray-600 text-center rounded-md overflow-hidden shadow-md max-w-sm mx-auto mt-10s w-11/12 max-h">
-        <div
-          className="h-[218px] w-80 mx-auto mt-5"
-          style={{
-            backgroundImage: "url('https://source.unsplash.com/500x500')",
-          }}
-        ></div>
-        <div className="bg-white w-36 mx-auto text-black rounded my-5">
-          {userRole}
-        </div>
+      <div className="relative text-center rounded-md overflow-hidden shadow-md max-w-sm mx-auto mt-10s w-11/12 max-h flex items-center justify-center">
+        <Image src={Card} alt="Card" className="w-full h-[350px]" />
+          <div className="absolute">
+            <div
+              className="h-[218px] w-80 mx-auto mt-5"
+              style={{
+              backgroundImage: "url('https://source.unsplash.com/500x500')",
+              }}
+            ></div>
+            <div className="bg-white w-36 mx-auto text-black rounded my-5">
+              {userRole}
+            </div>
+          </div>
       </div>
     </Fragment>
   );
