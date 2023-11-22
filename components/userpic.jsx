3c6af13d090471dc23327ad41b8b30d3cbcbd455
Wentonn <nicolas.owen@gmail.com>
2023-11-22 11:15:47 +0700

    userpic

--- components/userpic.jsx
+++ components/userpic.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import Card from "../public/card.png";
+import Image from "next/image";
 
-export default function Userpic() {
+export default function Userpic(props) {
+  // const [role, setRole] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("http://localhost:3000/api/profile")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       let role;
+  //       if (data.user.role === "NM") {
+  //         role = "Non-Member";
+  //       } else if (data.user.role === "M") {
+  //         role = "Member";
+  //       } else {
+  //         role = "Undefined";
+  //       }
+
+  //       setRole(role);
+  //       setLoading(false);
+  //     });
+  // }, []);
+  let userRole;
+
+  if (props.role === "NM") {
+    userRole = "Non-Member";
+  } else if (props.role === "M") {
+    userRole = "Member";
+  } else {
+    userRole = "Undefined";
+  }
+
   return (
-    <div className="bg-gray-600 text-center rounded-md overflow-hidden shadow-md max-w-sm mx-auto mt-10s w-11/12 max-h" >
-        <div className="h-[218px] w-80 mx-auto mt-5" style={{ backgroundImage:"url('https://source.unsplash.com/500x500')"}}>
-        </div>
-        <div className="bg-white w-36 mx-auto text-black rounded my-5">
-            Non-Member
-        </div>
-    </div>
-  )
+    <Fragment>
+      <div className="relative text-center rounded-md overflow-hidden shadow-md max-w-sm mx-auto mt-10s w-11/12 max-h flex items-center justify-center">
+        <Image src={Card} alt="Card" className="w-full h-[350px]" />
+          <div className="absolute">
+            <div
+              className="h-[218px] w-80 mx-auto mt-5"
+              style={{
+              backgroundImage: "url('https://source.unsplash.com/500x500')",
+              }}
+            ></div>
+            <div className="bg-white w-36 mx-auto text-black rounded my-5">
+              {userRole}
+            </div>
+          </div>
+      </div>
+    </Fragment>
+  );
 }
