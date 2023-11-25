30f9c5723973a0d129388ae1fb93c164f0c12bca
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-11-26 02:49:00 +0700

    404 responsif

--- pages/404.jsx
+++ pages/404.jsx
@@ -6,12 +6,12 @@ export default function NotFound() {
   return (
     <div className="">
       <Navbar />
-      <div className="flex">
-        <div className="w-1/2">
+      <div className="flex flex-col md:flex-row md:items-center">
+        <div className="w-full md:w-1/2">
           <Image src={NotFoundImage} alt="not found image" />
         </div>
-        <div className="w-2/5 px-20">
-          <h1 className="lg:text-7xl font-sans mb-10">We cant seem to find the page you're looking for</h1>
+        <div className="w-full md:w-2/5 px-20 mt-[50px]">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-sans mb-10">We cant seem to find the page you're looking for</h1>
           <a href="/" className="text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-4 py-2">
             Go Back
           </a>
