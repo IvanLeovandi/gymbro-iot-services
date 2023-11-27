72d2d8e1df49875b9bf6e2dfc841d1dabbe1f176
IvanLeovandi <16521021@std.stei.itb.ac.id>
2023-11-27 20:55:10 +0700

    install eslint and fix typing error on code based on eslint suggestion

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/navbar";
 import Image from "next/image";
 import NotFoundImage from "../public/not-found.png";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -11,10 +12,10 @@ export default function NotFound() {
           <Image src={NotFoundImage} alt="not found image" />
         </div>
         <div className="w-full md:w-2/5 px-20 mt-[50px]">
-          <h1 className="text-3xl md:text-5xl lg:text-7xl font-sans mb-10">We cant seem to find the page you're looking for</h1>
-          <a href="/" className="text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-4 py-2">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl font-sans mb-10">We cant seem to find the page you&apos;re looking for</h1>
+          <Link href="/" className="text-[#FFD700] font-sans border border-solid border-[#FFD700] rounded-lg px-4 py-2">
             Go Back
-          </a>
+          </Link>
         </div>
       </div>
     </div>
